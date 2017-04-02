238005b632aa48450fe3fda9028184182c322fb5
idanaftaker <idanaftaker@gmail.com>
2017-04-02 20:56:07 +0300

    first push

--- run.py
+++ run.py
@@ -1,21 +1,94 @@
+import datetime
 import urllib
-import re
 
 
-base_url = "https://finance.yahoo.com/q?s="
+class Quote(object):
+    DATE_FMT = '%Y-%m-%d'
+    TIME_FMT = '%H:%M:%S'
 
-list = ["aapl","amd","goog","nflx"]
-realNameList = ["Apple","Amd","Google","Netflix"]
+    def __init__(self):
+        self.symbol = ''
+        self.date, self.time, self.open_, self.high, self.low, self.close, self.volume = ([] for _ in range(7))
 
-i = 0
+    def append(self, dt, open_, high, low, close, volume):
+        self.date.append(dt.date())
+        self.time.append(dt.time())
+        self.open_.append(float(open_))
+        self.high.append(float(high))
+        self.low.append(float(low))
+        self.close.append(float(close))
+        self.volume.append(int(volume))
 
-while i < len(list):
-    url = base_url +list[i] +"&ql=1"
-    htmlfile = urllib.urlopen(url)
-    htmltext = htmlfile.read()
-    regex = '<span id="yfs_184_'+list[i] +'">(.+?)</span>'
-    pattern = re.compile(regex)
-    price = re.findall(pattern, htmltext)
-    print "The price of ", realNameList[i], " is: ", price
-    i += 1
+    def to_csv(self):
+        return ''.join(["{0},{1},{2},{3:.2f},{4:.2f},{5:.2f},{6:.2f},{7}\n".format(self.symbol,
+                                                                                   self.date[bar].strftime('%Y-%m-%d'),
+                                                                                   self.time[bar].strftime('%H:%M:%S'),
+                                                                                   self.open_[bar], self.high[bar],
+                                                                                   self.low[bar], self.close[bar],
+                                                                                   self.volume[bar])
+                        for bar in xrange(len(self.close))])
 
+    def write_csv(self, filename):
+        with open(filename, 'w') as f:
+            f.write(self.to_csv())
+
+    def read_csv(self, filename):
+        self.symbol = ''
+        self.date, self.time, self.open_, self.high, self.low, self.close, self.volume = ([] for _ in range(7))
+        for line in open(filename, 'r'):
+            symbol, ds, ts, open_, high, low, close, volume = line.rstrip().split(',')
+            self.symbol = symbol
+            dt = datetime.datetime.strptime(ds + ' ' + ts, self.DATE_FMT + ' ' + self.TIME_FMT)
+            self.append(dt, open_, high, low, close, volume)
+        return True
+
+    def __repr__(self):
+        return self.to_csv()
+
+
+
+class YahooQuote(Quote):
+  ''' Daily quotes from Yahoo. Date format='yyyy-mm-dd' '''
+  def __init__(self,symbol,start_date,end_date=datetime.date.today().isoformat()):
+    super(YahooQuote,self).__init__()
+    self.symbol = symbol.upper()
+    start_year,start_month,start_day = start_date.split('-')
+    start_month = str(int(start_month)-1)
+    end_year,end_month,end_day = end_date.split('-')
+    end_month = str(int(end_month)-1)
+    url_string = "http://ichart.finance.yahoo.com/table.csv?s={0}".format(symbol)
+    url_string += "&a={0}&b={1}&c={2}".format(start_month,start_day,start_year)
+    url_string += "&d={0}&e={1}&f={2}".format(end_month,end_day,end_year)
+    csv = urllib.urlopen(url_string).readlines()
+    csv.reverse()
+    for bar in xrange(0,len(csv)-1):
+      ds,open_,high,low,close,volume,adjc = csv[bar].rstrip().split(',')
+      open_,high,low,close,adjc = [float(x) for x in [open_,high,low,close,adjc]]
+      if close != adjc:
+        factor = adjc/close
+        open_,high,low,close = [x*factor for x in [open_,high,low,close]]
+      dt = datetime.datetime.strptime(ds,'%Y-%m-%d')
+      self.append(dt,open_,high,low,close,volume)
+
+
+
+# http://ichart.yahoo.com/table.csv?s=S58.SI&c=2009&a=9&b=23&f=2014&d=9&e=22&g=d&ignore=.csv
+#
+# The blue part is the stock symbol (only one symbol can be run at a time), the pink and green portion represent the start and end date respectively.
+# The brown portion is the interval in d,m, y.
+# By changing the interval g = v, the dividend information as in the dividend payout at the particular date is given. The url str is as below.
+
+# http://ichart.yahoo.com/table.csv?s=S58.SI&c=2009&a=9&b=23&f=2014&d=9&e=22&g=v&ignore=.csv
+#
+# For the script, the interval is easily set by using the following part of the code.
+# The formation of url will straight away append the hist price url and dividend url in a single function.
+
+
+if __name__ == '__main__':
+  q = YahooQuote('aapl','2017-04-01')              # download year to date Apple data
+  print q                                          # print it out
+  q = YahooQuote('orcl','2017-03-01','2017-04-3') # download Oracle data
+  q.write_csv('orcl.csv')                          # save it to disk
+  q = Quote()                                      # create a generic quote object
+  q.read_csv('orcl.csv')                           # populate it with our previously saved data
+  print q                                          # print it out
